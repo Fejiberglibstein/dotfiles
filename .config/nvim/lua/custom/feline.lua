96ba02453371d6023d8c167143b21a6bd0ad47c4
Fejiberglibstein <amthein321@gmail.com>
2024-07-02 01:02:23 -0400

    [nvim] Git support

--- .config/nvim/lua/custom/feline.lua
+++ .config/nvim/lua/custom/feline.lua
@@ -1,6 +1,9 @@
 local ctp_feline = require('catppuccin.groups.integrations.feline')
 ctp_feline.setup()
 
+
+-- Used to reload feline
+package.loaded['feline'] = nil
 require("feline").setup({
-    components = ctp_feline.get(),
+    components = ctp_feline.get()
 })
