959f78dca4e34a65bfc93b93b042ac86c030e890
Fejiberglibstein <amthein321@gmail.com>
2024-07-25 12:37:08 -0400

    [nvim] Add cmp telescope picker

--- .config/nvim/lua/custom/cmp.lua
+++ .config/nvim/lua/custom/cmp.lua
@@ -2,6 +2,77 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 luasnip.config.setup({})
 
+local function cmp_picker(opts)
+    opts = opts or {};
+
+    local picker = require('telescope.pickers');
+    local finder = require('telescope.finders');
+    local conf = require('telescope.config').values;
+    local previewer = require('telescope.previewers');
+    local actions = require('telescope.actions');
+    local actions_state = require('telescope.actions.state');
+
+    local results = cmp.get_entries()
+
+    picker.new(opts, {
+        prompt_title = 'Cmp options',
+        finder = finder.new_table({
+            results = results,
+            entry_maker = function(entry)
+                --- @cast entry cmp.Entry
+                return {
+                    value = entry,
+                    display = entry.completion_item.label,
+                    ordinal = entry.completion_item.label
+                };
+            end
+        }),
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr);
+
+                local option = {}
+                option.select = option.select or false
+                option.behavior = option.behavior or cmp.get_config().confirmation.default_behavior or
+                    cmp.ConfirmBehavior.Insert
+
+                local selection = actions_state.get_selected_entry().value;
+
+                cmp.core:confirm(selection, {
+                    behavior = option.behavior
+                }, function()
+                    cmp.core:complete(cmp.core:get_context({ reason = cmp.ContextReason.TriggerOnly }))
+                end)
+            end)
+
+            return true
+        end,
+
+        sorter = conf.generic_sorter(opts),
+        previewer = previewer.new_buffer_previewer({
+            title = 'LSP description',
+            define_preview = function(self, entry, _)
+                local value = entry.value
+                ---@cast value cmp.Entry
+                value:resolve(function()
+                    if not (value.resolved_completion_item and value.resolved_completion_item.documentation) then
+                        return
+                    end
+
+                    vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, value:get_documentation())
+                    vim.bo[self.state.bufnr].filetype = 'markdown'
+
+                    vim.wo[self.state.winid].conceallevel = 3
+                    vim.wo[self.state.winid].wrap = true
+                end)
+            end
+
+
+
+        })
+    }):find()
+end
+
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -21,6 +92,11 @@ cmp.setup({
         end, { 'i', 's' }),
         -- Select the prev item
         ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-b>'] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp_picker({})
+            end
+        end),
 
         -- Scroll the documentation window backwards and forwards
         ['<C-k>'] = cmp.mapping.scroll_docs(-4),
--- .config/nvim/lua/custom/plugins/cmp.lua
+++ .config/nvim/lua/custom/plugins/cmp.lua
@@ -1,15 +1,16 @@
-return { 
+return {
     'hrsh7th/nvim-cmp', -- https://github.com/hrsh7th/nvim-cmp
     event = 'InsertEnter',
     dependencies = {
         {
             -- https://github.com/L3MON4D3/LuaSnip
             'L3MON4D3/LuaSnip',
-            build =  'make install_jsregexp',
+            build = 'make install_jsregexp',
         },
         'saadparwaiz1/cmp_luasnip', -- https://github.com/saadparwaiz1/cmp_luasnip
-        'hrsh7th/cmp-nvim-lsp', -- https://github.com/hrsh7th/cmp-nvim-lsp
-        'hrsh7th/cmp-path', -- https://github.com/hrsh7th/cmp-path
+        'hrsh7th/cmp-nvim-lsp',     -- https://github.com/hrsh7th/cmp-nvim-lsp
+        'hrsh7th/cmp-path',         -- https://github.com/hrsh7th/cmp-path
+        'nvim-telescope/telescope.nvim',
     },
     config = function()
         require('custom.cmp')
