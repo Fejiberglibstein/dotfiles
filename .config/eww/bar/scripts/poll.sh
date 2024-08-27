7b0edd78db18648024b15179e6f534622667f70f
Austin Thein <amthein321@gmail.com>
2024-08-27 16:22:09 -0400

    Merge branch 'laptop-conf' into HEAD

--- .config/eww/bar/scripts/poll.sh
+++ .config/eww/bar/scripts/poll.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-bash $1
+eval $1
 socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-	bash $1
+	eval $1
 done
