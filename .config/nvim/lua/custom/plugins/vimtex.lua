19c175e8d63887faba7ea02fbcc8b5ab71b047ac
Austin Thein <amthein321@gmail.com>
2024-08-28 16:58:25 -0400

    Snippetd

---
+++ .config/nvim/lua/custom/plugins/vimtex.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "iurimateus/luasnip-latex-snippets.nvim",
+        dependencies = { "lervag/vimtex", "L3MON4D3/LuaSnip" },
+        ft = { "tex" },
+    },
+    {
+        "evesdropper/luasnip-latex-snippets.nvim",
+        ft = { "tex" },
+        dependencies = { "lervag/vimtex", "L3MON4D3/LuaSnip" },
+    },
+    {
+        "lervag/vimtex",
+        lazy = false, -- we don't want to lazy load VimTeX
+        -- tag = "v2.15", -- uncomment to pin to a specific release
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+            vim.g.vimtex_view_method = "zathura"
+            vim.api.nvim_create_autocmd("BufReadPre", {
+                pattern = { "*.tex" },
+                callback = function()
+                    vim.b.vimtex_main = vim.api.nvim_buf_get_name(0)
+                end
+            })
+        end
+    }
+}
