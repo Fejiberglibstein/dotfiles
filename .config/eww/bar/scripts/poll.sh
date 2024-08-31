606786c0453396f81e7364039b65a9fdbdea8bfc
Fejiberglibstein <amthein321@gmail.com>
2024-08-31 13:43:53 -0400

    (eww) fix workspace bar animation

--- .config/eww/bar/scripts/poll.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-eval $1
-socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-	eval $1
-done
--- .config/eww/bar/scripts/workspaces.sh
+++ .config/eww/bar/scripts/workspaces.sh
@@ -3,11 +3,19 @@
 
 active(){
 	hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id'
+	socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+		hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id'
+	done
 }
 
+LAST_VAL=""
 all() {
 	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
-	seq 1 10 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
+	VAL=$(seq 1 10 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})')
+	if [[ "$VAL" != "$LAST_VAL" ]]; then
+		echo "$VAL"
+		LAST_VAL="$VAL"
+	fi
 }
 
 case $1 in 
@@ -16,6 +24,9 @@ case $1 in
 		;;
 	"all")
 		all
+		socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+			all
+		done
 		;;
 esac
 
