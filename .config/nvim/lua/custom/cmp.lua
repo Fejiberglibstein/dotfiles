fd45b4597ac6c36b979ddaf29be298555b71d05d
Fejiberglibstein <amthein321@gmail.com>
2024-09-03 23:41:41 -0400

    nvim completiuon prrtyy

--- .config/nvim/lua/custom/cmp.lua
+++ .config/nvim/lua/custom/cmp.lua
@@ -78,27 +78,27 @@ local function cmp_picker(opts)
 end
 
 cmp.setup({
-    -- window = {
-    --     completion = {
-    --         winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-    --         col_offset = -1,
-    --         side_padding = 0,
-    --     },
-    -- },
-    -- formatting = {
-    --     expandable_indicator = true,
-    --     fields = { 'kind', 'abbr', 'menu' },
-    --     format = function(entry, vim_item)
-    --         --- @type vim.CompletedItem
-    --         local kind = require('lspkind').cmp_format({ mode = 'symbol_text', maxwidth = 50 })(entry, vim_item)
-    --
-    --         local strings = vim.split(kind.kind, "%s", { trimempty = true })
-    --         kind.kind = " " .. (strings[1] or "") .. " "
-    --         kind.menu = "    (" .. (strings[2] or "") .. ")"
-    --
-    --         return kind
-    --     end,
-    -- },
+    window = {
+        completion = {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -1,
+            side_padding = 0,
+        },
+    },
+    formatting = {
+        expandable_indicator = true,
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(entry, vim_item)
+            --- @type vim.CompletedItem
+            local kind = require('lspkind').cmp_format({ mode = 'symbol_text', maxwidth = 50 })(entry, vim_item)
+
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. (strings[1] or "") .. " "
+            kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+            return kind
+        end,
+    },
 
     snippet = {
         expand = function(args)
