a9dee9fd28afb6d9ef8f96253fdc273d0c7c9a2b
Fejiberglibstein <amthein321@gmail.com>
2024-09-19 16:48:26 -0400

    Fix markdown rendering

--- .config/nvim/lua/custom/plugins/school.lua
+++ .config/nvim/lua/custom/plugins/school.lua
@@ -2,10 +2,12 @@ return {
     {
         'MeanderingProgrammer/render-markdown.nvim',
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ft = { "markdown" },
         opts = {
             overrides = { buftype = { nofile = { enabled = false } } },
             code = { left_pad = 1, },
             latex = { enabled = false },
+            ft = { "markdown" },
             heading = {
                 left_pad = 1,
                 sign = false,
