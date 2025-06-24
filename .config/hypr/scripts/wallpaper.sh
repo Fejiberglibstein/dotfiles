b87afd45e713ece584e3c828c67f4e7f65134ffd
Austin Thein <amthein321@gmail.com>
2025-06-23 23:45:48 -0400

    hyprland stuff

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-result=$(find ~/wallpapers/ |
+result=$(find ~/wallpapers/ -type f \! -name "*_blur.*" |
 	sed 's/\/home\/'$USER'/~/' |
 	xargs -I {} echo -en "{}\0icon\x1f{}\n" | 
 	shuf |
-	rofi -dmenu \
-	-theme-str '@import "wallpaper.rasi"' \
+	rofi -dmenu -theme-str '@import "wallpaper.rasi"' |
+	sed 's/~/\/home\/'$USER'/'
 )
 
 if [ -n "$result" ]; then
-	hyprctl hyprpaper unload all
-	hyprctl hyprpaper preload "$result"
-	hyprctl hyprpaper wallpaper ", $result"
+	magick "${result}" -scale 11% -blur 0x4 ~/wallpapers/tmp.png;
+	hyprctl hyprpaper unload all;
+	hyprctl hyprpaper preload "~/wallpapers/tmp.png";
+	hyprctl hyprpaper wallpaper ", ~/wallpapers/tmp.png"
 fi
