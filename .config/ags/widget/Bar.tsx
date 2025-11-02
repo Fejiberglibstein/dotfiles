9c9694ba7dbc4a6e2dac5dbae861de9948229a3b
Austin Thein <amthein321@gmail.com>
2025-11-02 12:37:37 -0500

    More ags

--- .config/ags/widget/Bar.tsx
+++ .config/ags/widget/Bar.tsx
@@ -1,6 +1,7 @@
 import { App, Astal, Gtk, Gdk } from "astal/gtk3"
 import { Binding, Variable, bind } from "astal"
 import Hyprland from "gi://AstalHyprland"
+import Battery from "gi://AstalBattery"
 import { range, Bbox } from "./util"
 
 const time = Variable("").poll(5000, "date")
@@ -15,7 +16,7 @@ function Workspaces() {
 
     const selected = bind(hypr, "focusedWorkspace").as(fs => fs.id)
 
-    return <box spacing="4" halign="baseline" space-evenly="false" >
+    return <box spacing="4" space-evenly="false" className="container">
         {
             range(10).map(i => i + 1).map(i => {
 
@@ -41,6 +42,29 @@ function Workspaces() {
     </box>
 }
 
+function BatteryLevel() {
+    const bat = Battery.get_default()
+
+    return <box className="container"
+        visible={bind(bat, "isPresent")}>
+        <icon icon={bind(bat, "batteryIconName")} />
+        {bind(bat, "percentage").as(p =>
+            `${Math.floor(p * 100)} %`
+        )}
+    </box>
+}
+
+function Content(props: { side, child, children }) {
+    return <box
+        spaceEvenly={false}
+        halign={props.side}
+        valign={Gtk.Align.CENTER}
+        spacing={10}
+    >
+        {props.children ?? props.child}
+    </box>
+}
+
 export default function Bar(gdkmonitor: Gdk.Monitor) {
     const { TOP, LEFT, RIGHT, BOTTOM } = Astal.WindowAnchor
 
@@ -50,11 +74,19 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
         exclusivity={Astal.Exclusivity.EXCLUSIVE}
         anchor={TOP | LEFT | RIGHT}
         application={App}>
-        <centerbox orientation="h">
-            <box orientation="v">
+        <centerbox hexpand={Gtk.Align.CENTER}>
+            <Content side={Gtk.Align.LEFT}>
+                <BatteryLevel />
+            </Content>
+
+            <Content side={Gtk.Align.CENTER}>
                 <Workspaces />
-            </box>
-            <box>{time()}</box>
+            </Content>
+
+            <Content side={Gtk.Align.RIGHT}>
+                <box className="container">{time()}</box>
+            </Content>
+
         </centerbox>
     </window>
 }
