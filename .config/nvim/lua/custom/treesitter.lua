390afea875a9cca2ad35dbe5805b0d03a704aa0f
Austin Thein <amthein321@gmail.com>
2026-01-30 00:06:45 -0500

    Fix treesitter

--- .config/nvim/lua/custom/treesitter.lua
+++ .config/nvim/lua/custom/treesitter.lua
@@ -1,75 +1,81 @@
 ---@diagnostic disable: missing-fields
-require('nvim-treesitter').setup()
 
-require('nvim-treesitter.config').setup {
-    auto_install = true,
-    highlight = {
-        enable = true,
-        disable = { "asm" },
-        additional_vim_regex_highlighting = false
-    }
-}
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = require("nvim-treesitter").get_installed(),
+    callback = function()
+        vim.treesitter.start()
+    end,
+})
 
-require 'nvim-treesitter.config'.setup {
-    textobjects = {
-        select = {
-            enable = true,
+-- require("nvim-treesitter.config").setup({
+--     auto_install = true,
+--     highlight = {
+--         enable = true,
+--         disable = { "asm" },
+--         additional_vim_regex_highlighting = false,
+--     },
+-- })
 
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
+-- require("nvim-treesitter.config").setup({
+--     textobjects = {
+--         select = {
+--             enable = true,
+--
+--             -- Automatically jump forward to textobj, similar to targets.vim
+--             lookahead = true,
+--
+--             keymaps = {
+--                 -- You can use the capture groups defined in textobjects.scm
+--                 ["af"] = "@function.outer",
+--                 ["if"] = "@function.inner",
+--                 ["as"] = "@block.outer",
+--                 ["is"] = "@block.inner",
+--
+--                 -- ["ac"] = "@class.outer",
+--
+--                 -- You can optionally set descriptions to the mappings (used in the desc parameter of
+--                 -- nvim_buf_set_keymap) which plugins like which-key display
+--
+--                 -- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+--
+--                 -- You can also use captures from other query groups like `locals.scm`
+--                 -- ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+--             },
+--             -- You can choose the select mode (default is charwise 'v')
+--             --
+--             -- Can also be a function which gets passed a table with the keys
+--             -- * query_string: eg '@function.inner'
+--             -- * method: eg 'v' or 'o'
+--             -- and should return the mode ('v', 'V', or '<c-v>') or a table
+--             -- mapping query_strings to modes.
+--             selection_modes = {
+--                 ["@parameter.outer"] = "v", -- charwise
+--                 ["@function.outer"] = "V", -- linewise
+--                 -- ['@class.outer'] = '<c-v>', -- blockwise
+--             },
+--             -- If you set this to `true` (default is `false`) then any textobject is
+--             -- extended to include preceding or succeeding whitespace. Succeeding
+--             -- whitespace has priority in order to act similarly to eg the built-in
+--             -- `ap`.
+--             --
+--             -- Can also be a function which gets passed a table with the keys
+--             -- * query_string: eg '@function.inner'
+--             -- * selection_mode: eg 'v'
+--             -- and should return true or false
+--             include_surrounding_whitespace = true,
+--         },
+--     },
+-- })
 
-            keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["as"] = "@block.outer",
-                ["is"] = "@block.inner",
-
-                -- ["ac"] = "@class.outer",
-
-                -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                -- nvim_buf_set_keymap) which plugins like which-key display
-
-                -- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-
-                -- You can also use captures from other query groups like `locals.scm`
-                -- ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-            },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
-            selection_modes = {
-                ['@parameter.outer'] = 'v', -- charwise
-                ['@function.outer'] = 'V',  -- linewise
-                -- ['@class.outer'] = '<c-v>', -- blockwise
-            },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true or false
-            include_surrounding_whitespace = true,
-        },
-    },
-}
-
-local parser_configs = require("nvim-treesitter.parsers")
-
-parser_configs.comment = {
-    install_info = {
-        url = "https://github.com/OXY2DEV/tree-sitter-comment",
-        files = { "src/parser.c" },
-        branch = "main",
-
-        -- Also installs the query files(*syntax highlighting*), Only for the `main` branch of `nvim-treesitter`.
-        queries = "queries/"
-    },
-}
+-- local parser_configs = require("nvim-treesitter.parsers")
+--
+-- parser_configs.comment = {
+--     install_info = {
+--         url = "https://github.com/OXY2DEV/tree-sitter-comment",
+--         files = { "src/parser.c" },
+--         branch = "main",
+--
+--         -- Also installs the query files(*syntax highlighting*), Only for the `main` branch of `nvim-treesitter`.
+--         queries = "queries/",
+--     },
+-- }
